9d71aaffeb71b1091ea8100121bd5e6fb8bf4227
sanchayarun <sanchayarun@gmail.com>
2022-09-06 11:22:31 +0100

    connection to mogodb database and port

--- server/index.js
+++ server/index.js
@@ -6,3 +6,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+// Connection to the database
+mongoose.connect('mongodb://127.0.0.1:27017/qacinema', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to movie Directory"))
+.catch(console.error);
+
+// Listening to the port
+const server = app.listen(3002, () => console.log(`The server has been activated on ${server.address().port}`));
