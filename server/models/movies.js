06d3cc02368d6d1f3937487d8333035df5a8beb1
sanchayarun <sanchayarun@gmail.com>
2022-09-06 10:44:54 +0100

    movies and showing time schema created

---
+++ server/models/movies.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const ShowingTimes = require("./showingtimes");
+const Schema = mongoose.Schema;
+
+
+const MoviesSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    release_date: {
+        type: Date,
+        required: true
+    },
+
+    directors: {
+        type: Array,
+        required: true
+    },
+
+    actors: {
+        type: Array,
+        required: true
+    },
+
+    showing_times: {
+        times: [ShowingTimes]
+    },
+
+    duration: {
+        type: String,
+        required: true
+    },
+
+    rating: {
+        type: String,
+        enum: ["Uc","U", "PG", "12", "12A", "15", "18", "R18"],
+        required: true
+    },
+
+    poster: {
+        type: String,
+        required:true
+    }
+
+
+
+})
+
+
+const Movies = mongoose.model("Movies", MoviesSchema);
+module.exports = Movies;
+
+
+
