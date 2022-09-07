a6dc67a161445eeae0d1a9ab541f47a45e47baad
sanchayarun <sanchayarun@gmail.com>
2022-09-07 10:34:36 +0100

    Payments Schema created

---
+++ server/models/payments.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const PaymentSchema = new Schema({
+
+
+    firstName: {
+        type: String,                                                                                           
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    cardNumber: {
+        type: Number,
+        required: true
+    },
+
+    expiryDate: {
+        type: Date,
+        required: true
+    },
+
+    SecurityCode: {
+        type: Number,
+        required: true
+    },
+
+    timestamp: true 
+
+
+})
+
+
+const Payments = model("Payments", PaymentSchema);
+
+module.exports = {"Payments" : Payments};
