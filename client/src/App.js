7fc2e3cc47ab16562e13cf4c83f6eab4a9b953e6
abdirizak osman <abdirizakosman123@gmail.com>
2022-09-07 15:25:20 +0100

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+// import './App.css';
+import GettingTherePage from './components/GettingThere';
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>WELCOME TO CINEMA</h1>
+      <GettingTherePage />
     </div>
+    </>
   );
 }
 
---
+++ client/src/components/About/Developers/Charlie.jsx
@@ -0,0 +1,22 @@
+const Charles = () => {
+    return (
+      <>
+      <div className="">
+        <div className="">
+          <h3>Abdi</h3>
+          <p>
+            Hi, my name is Charles.. 
+          </p>
+
+          <a href="">
+          </a> <br />
+
+          <a href=""> </a>
+            
+        
+        
+      </div>
+      </div>
+
+
+    </> )};
