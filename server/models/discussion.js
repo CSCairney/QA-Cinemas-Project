0855ef71348a70ebd05287cbd43b05c11ab145d0
CSCairney <charlescairney@yahoo.co.uk>
2022-09-07 10:48:07 +0100

    Discussion Schema and routing complete

---
+++ server/models/discussion.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const DiscussionSchema = new Schema({
+
+    username: {
+        type: String,
+        required: true
+    },
+
+    timestamps: true,
+
+    subjectMovie: {
+        type: String,
+        required: true
+    },
+
+    rating: {
+        type: String,
+        enum: ["1","2", "3", "4", "5"],
+        required: true
+    },
+
+    spoilerMarker: {
+        type: Boolean,
+        required: false
+    },
+
+    message: {
+        type: String,
+        required: true
+    }
+   
+
+
+})
+
+const Discussion = model("Discussion", DiscussionSchema);
+
+module.exports = {"Discussion" : Discussion};
