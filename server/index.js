e6a895eb57ccdf6d709a4e602e29a8a74a429886
sanchayarun <sanchayarun@gmail.com>
2022-09-07 11:47:19 +0100

    payment routes added

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const moviesRouter = require ('./routes/movies.js');
+const paymentRouter = require ('./routes/payments.js')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use('/movies', moviesRouter);
+app.use('/payments', paymentRouter);
 
 // Connection to the database
 mongoose.connect('mongodb://127.0.0.1:27017/qacinema', {
