45223df9fe94b89c1c83d49f76b8404afc07bfa5
abdirizak osman <abdirizakosman123@gmail.com>
2022-09-08 12:32:59 +0100

    deleted lines

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
-import './App.css';
-import GettingTherePage from './components/GettingThere';
 
 function App() {
   return (
-    <>
     <div className="App">
       <h1>WELCOME TO CINEMA</h1>
-      <GettingTherePage />
     </div>
-    </>
   );
 }
 
