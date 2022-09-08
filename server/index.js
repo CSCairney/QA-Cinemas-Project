64f5a7bc83f6764e8f9dec8005fdc8d021ff9fc2
Mateusz Kluska <mateusz.marek.kluska@gmail.com>
2022-09-08 10:15:23 +0100

    updated index.js, created controllers

--- server/index.js
+++ server/index.js
@@ -2,29 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const moviesRouter = require ('./routes/movies.js');
-const paymentsRouter = require ('./routes/payments.js');
-const bookingsRouter = require ('./routes/bookings.js');
-const discussionRouter = require ('./routes/discussion.js');
-
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+//Routers imports
+const moviesRouter = require('./routes/moviesRouter.js');
+const paymentsRouter = require('./routes/paymentsRouter.js');
+const bookingsRouter = require('./routes/bookingsRouter.js');
+const discussionsRouter = require('./routes/discussionsRouter.js');
+
+//Default paths for routers
 app.use('/movies', moviesRouter);
 app.use('/payments', paymentsRouter);
 app.use('/bookings', bookingsRouter);
-app.use('/discussion', discussionRouter);
-
+app.use('/discussions', discussionsRouter);
 
 // Connection to the database
 mongoose.connect('mongodb://127.0.0.1:27017/qacinema', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log("Connected to movie Directory"))
-.catch(console.error);
+    .then(() => console.log("Connected to movie Directory"))
+    .catch(console.error);
 
 // Listening to the port
-const server = app.listen(3002, () => console.log(`The server has been activated on ${server.address().port}`));
+const server = app.listen(3002, () =>
+    console.log(`The server has been activated on ${server.address().port}`));
+    
