513944fd6496ebc57d0bf1445891059b31909543
Mateusz Kluska <mateusz.marek.kluska@gmail.com>
2022-09-08 09:46:58 +0100

    Creating Controllers

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 //Routers imports
 const moviesRouter = require('./routes/moviesRouter.js');
 const paymentsRouter = require('./routes/paymentsRouter.js');
 const bookingsRouter = require('./routes/bookingsRouter.js');
 const discussionsRouter = require('./routes/discussionsRouter.js');
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 //Default paths for routers
 app.use('/movies', moviesRouter);
@@ -27,4 +27,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/qacinema', {
     .catch(console.error);
 
 // Listening to the port
-const server = app.listen(3002, () => console.log(`The server has been activated on ${server.address().port}`));
+const server = app.listen(3002, () =>
+    console.log(`The server has been activated on ${server.address().port}`));
