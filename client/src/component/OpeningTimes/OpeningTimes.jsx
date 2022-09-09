740c1f971f51a0d4b2f9eb6ac84d50a317d1e59c
abdirizak osman <abdirizakosman123@gmail.com>
2022-09-09 15:36:18 +0100

    initial commit

---
+++ client/src/component/OpeningTimes/OpeningTimes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const OpeningTimes = () => {
+    return (
+        <div className="container demo-bg">
+<div className="row">
+{/* <div className="col-sm-4"></div>
+<div className="col-sm-4"></div> */}
+<div className="col-sm-4">
+<div className="business-hours">
+<h2 className="title">Opening Hours</h2>
+<ul className="list-unstyled opening-hours">
+<li>Sunday <span class="pull-right">12:00-18:00</span></li>
+<li>Monday <span className="pull-right">10:00-22:00</span></li>
+<li>Tuesday <span classNmae="pull-right">10:00-22:00</span></li>
+<li>Wednesday <span className="pull-right">10:00-22:00</span></li>
+<li>Thursday <span className="pull-right">10:00-22:00</span></li>
+<li>Friday <span className="pull-right">10:00-22:00</span></li>
+<li>Saturday <span className="pull-right">10:00-22:00</span></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+    )
+}
+export default OpeningTimes;
