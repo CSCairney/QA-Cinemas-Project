d69952cd3b5d967f5b2041f3f9e1f9d5a3837aa2
CSCairney <charlescairney@yahoo.co.uk>
2022-09-09 12:39:36 +0100

    forums states

--- client/src/component/discussion/discussionsMessages.jsx
+++ client/src/component/discussion/discussionsMessages.jsx
@@ -4,98 +4,88 @@ import {useState, useEffect } from "react";
 
 
 
-function discussionMessages() {
+const Discussion = () => {
     const [discussions, setDiscussions] = useState([]);
-    const [discussionUsername, setDiscussionUsername] = useState([]);
-    const [discussionSubjectMovie, setDiscussionSubjectMovie] = useState([]);
-    const [discussionRating, setDiscussionRating] = useState([]);
-    const [discussionSpoilerMarker, setDiscussionSpoilerMarker] = useState([]);
-    const [discussionMessage, setDiscussionMessage] = useState([]);
+    const [discussionUsername, setDiscussionUsername] = useState("");
+    const [discussionSubjectMovie, setDiscussionSubjectMovie] = useState("");
+    const [discussionRating, setDiscussionRating] = useState("");
+    const [discussionSpoilerMarker, setDiscussionSpoilerMarker] = useState(false);
+    const [discussionMessage, setDiscussionMessage] = useState("");
 
     const addDiscussionMessage = (e) =>{
             e.preventDefault()
-            const discussion = {discussionUsername, discussionSubjectMovie, discussionRating, discussionSpoilerMarker, discussionMessage}
+            const discussion = {
+                username: discussionUsername,
+                subjectMovie: discussionSubjectMovie,
+                rating: discussionRating,
+                spoilerMarker: discussionSpoilerMarker,
+                message: discussionMessage
+            }
             console.log(discussion)
-            fetch(axios.post('localhost:3002/discussion/create',{
+            axios.post('localhost:3002/discussions/create',{
                 body:JSON.stringify(discussion)
-            }))
-            fetchDiscussionMessages();
+            }).then(()=>{
+                console.log("New discussion message added")
+                
+            })
+            
     }
 
     useEffect(() => {
-        const fetchDiscussionMessages = async () => {
-            try {
-                const response = await axios.get('localhost:3002/discussion/getAll');
-                setDiscussions(response.data);
-            } catch (err) {
-                if (err.response) {
-                    console.log(err.response.data);
-                    console.log(err.response.status);
-                    console.log(err.response.headers);
-                } else {
-                    console.log(`Error: ${err.message}`);
-                }
 
-            }
-        }
+        axios.get('localhost:3002/discussions/getAll')
+            .then(res => res.json())
+            .then((result) => {
+                setDiscussions(result);
+            })
 
-        fetchDiscussionMessages();
+
     }, [])
-  
-    // const handleDelete = async (id) => {
-    //     try {
-    //         await api.delete(`/${id}`);
-    //         const discussionsList = discussions.filter(discussion => discussion.id !== id);
-    //         setDiscussions(discussionsList);
-    //         history.pushState('/getAll');
-    //     } catch (err) {
-    //         console.log(`Error: ${err.message}`);
-    //     }
-    // }
-      
+          
         return (
             
-                <div onload={fetchDiscussionMessages}>
-                    <form onSubmit={DiscussionHandler}>
-                        <div class="mb-3">
-                            <label for="usernameInput1" class="form-label">Username:</label>
-                            <input type="text" class="form-control" id="usernameInput" aria-describedby="usernameInput" placeholder="Please input cinema username." value={discussionUsername} onChange={(e)=>setDiscussionUsername(e.target.value)}></input>
-                            
-                        </div>
-                        <div class="mb-3">
-                            <label for="subjectMovieInput1" class="form-label">Subject Movie:</label>
-                            <input type="text" class="form-control" id="subjectMovieInput" aria-describedby="subjectMovieInput" placeholder="Please input movie title." value={discussionSubjectMovie} onChange={(e)=>setDiscussionSubjectMovie(e.target.value)}></input>
-                        </div>
-                                         <div class="mb-3">
-                            <label for="ratingInput1" class="form-label">Rating:</label>
-                            <div id="ratingHelp" class="form-text">1-5.</div>
-                            <select id="ratingSelectValue" value={discussionRating} onChange={(e)=>setDiscussionRating(e.target.value)}>
-                                <option selected disabled>select</option>
-                                <option value="1">1 - Poor</option>
-                                <option value="2">2 - Okay</option>
-                                <option value="3">3 - Good</option>
-                                <option value="4">4 - Great</option>
-                                <option value="5">5 - Perfect</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="messageInput1" class="form-label">Message</label>
-                            <input type="text" class="form-control" id="subjectMovieInput" aria-describedby="subjectMovieInput" placeholder="Discussion message here." value={discussionMessage} onChange={(e)=>setDiscussionMessage(e.target.value)}></input>
-                        </div>
-                        <button type="submit" class="btn btn-primary" onClick={addDiscussionMessage}>Submit</button>
-                    </form>
+            <div>
+                <form onSubmit={addDiscussionMessage}>
+                    <div className="mb-3">
+                        <label htmlFor="usernameInput1" className="form-label">Username:</label>
+                        <input type="text" className="form-control" id="usernameInput" aria-describedby="usernameInput" placeholder="Please input cinema username." value={discussionUsername} onChange={(e) => setDiscussionUsername(e.target.value)}></input>
 
-                    <div className="discussionResult">
-                        <button onClick={this.deleteDiscussion}>Delete Discussion</button>
-                        <ul>
-                            {
-                                this.state.discussions.map(discussion => <li key={discussion.id}>'Username: {discussion.username}<br /> Rating: {discussion.rating}<br />  {discussion.message}'</li>)
-                            }
-                        </ul>
                     </div>
+                    <div className="mb-3">
+                        <label htmlFor="subjectMovieInput1" className="form-label">Subject Movie:</label>
+                        <input type="text" className="form-control" id="subjectMovieInput" aria-describedby="subjectMovieInput" placeholder="Please input movie title." value={discussionSubjectMovie} onChange={(e) => setDiscussionSubjectMovie(e.target.value)}></input>
+                    </div>                    
+                    <div className="mb-3">
+                        <label htmlFor="messageInput1" className="form-label">Message</label>
+                        <input type="text" className="form-control" id="subjectMovieInput" aria-describedby="subjectMovieInput" placeholder="Discussion message here." value={discussionMessage} onChange={(e) => setDiscussionMessage(e.target.value)}></input>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="ratingInput1" className="form-label">Rating:</label>
+                        <select multiple={false} id="ratingSelectValue" value={discussionRating} onChange={(e) => setDiscussionRating(e.target.value)}>
+                            <option selected disabled>select</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+
+                <div className="discussionResult">
+                    <button className="btn btn-primary">Delete Discussion</button>
+                    <ul>
+                        {
+                            discussions.map(discussion => <li key={discussion.id}>'Username: {discussion.username}<br /> Rating: {discussion.rating}<br />  {discussion.message}'</li>)
+                        }
+                    </ul>
                 </div>
+            </div>
             
         )
     }
+
+    export default Discussion;
 
 
