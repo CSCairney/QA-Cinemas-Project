3807aadb668aed7555bd378dc4661fb5aae8cfdf
Mateusz Kluska <mateusz.marek.kluska@gmail.com>
2022-09-10 09:53:01 +0100

    Created custom routes

--- server/routes/bookingsRouter.js
+++ server/routes/bookingsRouter.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const bookingsController = require("../controllers/bookingsController.js");
 
-// Get request for bookings,
+// Read request for all bookings,
 router.get("/getAll", bookingsController.getAllBookings);
 
-// Get request for bookings by ID,
-router.get("/getById/:id", bookingsController.getById );
+// Read request for bookings by ID,
+router.get("/getById/:id", bookingsController.getById);
 
-// Post request for bookings,
-router.post("/create", bookingsController.createBookings );
+// Create request for bookings,
+router.post("/create", bookingsController.createBookings);
 
-// Put request for bookings,
+// Update request for bookings,
 router.put("/update/:id", bookingsController.updateBookings);
 
 // Delete request for bookings,
