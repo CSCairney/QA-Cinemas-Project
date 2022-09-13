5787d25f7ee88f74718248889255b52477e6ae15
CSCairney <charlescairney@yahoo.co.uk>
2022-09-13 17:26:10 +0100

    working format

--- client/src/component/listing/listingTotal.jsx
+++ client/src/component/listing/listingTotal.jsx
@@ -8,7 +8,7 @@ const ListingTotal = () => {
     const [movies, setMovies] = useState([]);
     // const [showAccordion, setShowAccordion] = useState(true);
     // this.toggleAccordion = this.toggleAccordion.bind(this);
-
+    
     useEffect(() => {
 
         axios.get('http://localhost:3002/movies/getAll')
@@ -23,41 +23,42 @@ const ListingTotal = () => {
 
         return (
             <div className="accordion">
-                {movies?.map((movie) => (
+                {movies?.map((movie) => (                    
                     <div className="accordion-item" key={movie._id}>
                         <h2 className="accordion-header" id="headingOne">
-                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <button className="accordion-button" type="button">
                             </button>
                         </h2>
-                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div id="collapseOne" className="accordion-collapse collapse show">
                             <div className="accordion-body" id="listingItem">
-                                <div className="listingTitle">
-                                    <h3 id="listingTitle">{movie.title}</h3>
-                                </div>
-                                <div className="listingWidePoster">
-                                    <img src={movie.widePoster} alt="wide movie poster"></img>
-                                </div>                               
-                                <div className="listingDetails">
+                                <div className="listingTextArea">
+                                    <div className="listingMainDetails">
+                                        <div className="listingTitle">
+                                            <h3 id="listingTitle">{movie.title}</h3>
+                                        </div>
+                                        <div className="listingDetails">
+                                            <h5><strong>Classification: </strong> {movie.rating}</h5>
+                                            <h5><strong>Release Date: </strong> {movie.release_date}</h5>
+                                        </div>
                                     
-                                    <h5><strong>Classification: </strong> {movie.rating}</h5>
-                                    <h5><strong>Release Date: </strong> {movie.release_date}</h5>
-                                </div>
-                                <div className="listingStaff">
-                                    <p><strong>Directors:</strong> {movie.directors}</p>
-                                    <p><strong>Actors: </strong>{movie.actors}</p>
-                                </div>
-                                <div className="listingDescription">
-                                    <p className="listingDescription">{movie.description}</p>
-                                </div>    
+                                    <div className="listingStaff">
+                                        <p><strong>Directors:</strong> {movie.directors}</p>
+                                        <p><strong>Actors: </strong>{movie.actors}</p>
+                                    </div>
+                                    <div className="listingDescription">
+                                        <p className="listingDescription">{movie.description}</p>
+                                    </div>  
+                                    </div>
+                                </div>  
                                 <div className="listingTrailer">
-                                    <iframe width="1120" height="630" src={movie.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="1120" height="630" src={movie.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                                 </div>                            
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
-
+            
 
         )
 
