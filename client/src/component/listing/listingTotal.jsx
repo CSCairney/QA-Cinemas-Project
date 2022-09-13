981cc51f81f26ba910f935aa581855f30f5a1474
CSCairney <charlescairney@yahoo.co.uk>
2022-09-13 16:20:25 +0100

    styling added

--- client/src/component/listing/listingTotal.jsx
+++ client/src/component/listing/listingTotal.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from 'axios';
-import './listing.css';
+import './listingTotal.css';
 import {useState, useEffect } from "react";
 
 
 const ListingTotal = () => {
     const [movies, setMovies] = useState([]);
+    // const [showAccordion, setShowAccordion] = useState(true);
+    // this.toggleAccordion = this.toggleAccordion.bind(this);
 
     useEffect(() => {
 
@@ -25,20 +27,31 @@ const ListingTotal = () => {
                     <div className="accordion-item" key={movie._id}>
                         <h2 className="accordion-header" id="headingOne">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                {movie.title}
                             </button>
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div className="accordion-body">
-                                <img src={movie.widePoster}></img>
-                                <h3>{movie.title}</h3>
-                                <h5>{movie.rating}</h5>
-                                <h5>{movie.release_date}</h5>
-                                <p>{movie.directors}</p>
-                                <p>{movie.actors}</p>
-                                <p>{movie.description}</p>
-                                <iframe width="560" height="315" src={movie.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                
+                            <div className="accordion-body" id="listingItem">
+                                <div className="listingTitle">
+                                    <h3 id="listingTitle">{movie.title}</h3>
+                                </div>
+                                <div className="listingWidePoster">
+                                    <img src={movie.widePoster} alt="wide movie poster"></img>
+                                </div>                               
+                                <div className="listingDetails">
+                                    
+                                    <h5><strong>Classification: </strong> {movie.rating}</h5>
+                                    <h5><strong>Release Date: </strong> {movie.release_date}</h5>
+                                </div>
+                                <div className="listingStaff">
+                                    <p><strong>Directors:</strong> {movie.directors}</p>
+                                    <p><strong>Actors: </strong>{movie.actors}</p>
+                                </div>
+                                <div className="listingDescription">
+                                    <p className="listingDescription">{movie.description}</p>
+                                </div>    
+                                <div className="listingTrailer">
+                                    <iframe width="1120" height="630" src={movie.trailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>                            
                             </div>
                         </div>
                     </div>
