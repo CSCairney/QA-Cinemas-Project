dc13d2bb0f0d7ba04fa376438098e759cbbc9298
LuxshanPuvanendrarajah <Luxpuvan@gmail.com>
2022-09-14 14:37:28 +0100

    routing basic and developing nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import Discussion from "./component/discussion/discussionsMessages";
-// import EmailForm from './component/contact/emailForm';
-// import PaymentForm from './component/payment/payment';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactUs from "./Pages/contactus";
+import Cinemanavbar from "./component/Cinemanavbar/cinemanavbar";
+import './App.css'
 
 function App() {
   return (
-    <>
-    <Discussion/>
-    </>
+    <Router>
+      <Cinemanavbar/>
+      <Routes>
+        <Route path="/ContactUs" element= {<ContactUs/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-// import App from './App';
-// import EmailForm from './component/contact/emailForm';
-// import PaymentModal from './component/payment/paymentModal';
-// import Discussion from './component/discussion/discussionsMessages';
-// import BookingForm from './component/booking/bookings';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-    {/* <App /> */}
-    {/* <EmailForm/>*/}
-    {/* <PaymentModal/>  */}
-    {/* <Discussion/>*/}
-    {/* <BookingForm/>  */}
-
+     <App/> 
     </>
   </React.StrictMode>
 );
