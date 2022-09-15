03af6932667be2f444de8f1e964a2a90feb76d8b
dnmihnea <mihnea.dinu100@gmail.com>
2022-09-15 23:22:13 +0100

    Added booking to payment connection

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+// import App from './App';
 // import EmailForm from './component/contact/emailForm';
 // import PaymentModal from './component/payment/paymentModal';
 // import Discussion from './component/discussion/discussionsMessages';
-// import BookingForm from './component/booking/bookings';
+import BookingForm from './component/booking/bookings';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
@@ -28,7 +28,7 @@ root.render(
     <>
 
 
-     <App/> 
+     <BookingForm/> 
      
 
     </>
